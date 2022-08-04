cedda1ec347b8233f68f1d6b17ef6548966b30b3
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-04 19:38:02 +0530

    check user email is valid or not

--- src/com/bridgelabz/UserRegistrationRegex.java
+++ src/com/bridgelabz/UserRegistrationRegex.java
@@ -1,33 +1,52 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationRegex {
-    public void checkFirstName(String name) {
-        boolean isMatched = Pattern.compile("^[A-Z][a-zA-Z]{2,}").matcher(name).matches();
-        if (isMatched) {
-            System.out.println("It is valid name");
-        } else {
-            System.out.println("It is not a valid name");
-        }
+    public static void main(String[] args) {
+        UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first name");
+        String firstName = sc.next();
+        userRegistrationRegex.checkFirstName(firstName);
+        System.out.println();
+
+        System.out.println("Enter last name");
+        String lastName = sc.next();
+        userRegistrationRegex.checkLastName(lastName);
+
+        System.out.println("Enter email");
+        String checkEmail = sc.next();
+        userRegistrationRegex.checkEmail(checkEmail);
     }
 
-    public void checkLastName(String lastName) {
-        boolean isMatched = Pattern.compile("^[A-Z][a-zA-Z]{2,}").matcher(lastName).matches();
-        if (isMatched) {
-            System.out.println("It is valid name");
-        } else {
-            System.out.println("It is not a valid name");
-        }
+    public void checkFirstName (String firstName){
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+        Matcher match = pattern.matcher(firstName);
+        if (match.matches())
+            System.out.println("First name is Valid");
+        else
+            System.out.println("First name is InValid");
     }
 
-    public void checkEmail(String email) {
-        boolean isMatched = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.]+$").matcher(email).matches();
-        if(isMatched){
-            System.out.println("It is valid email");
-        }
-        else{
-            System.out.println("It is not a valid email");
-        }
+    public void checkLastName (String lastName){
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+        Matcher match = pattern.matcher(lastName);
+        if (match.matches())
+            System.out.println("Last name is Valid");
+        else
+            System.out.println("Last name is InValid");
+    }
+
+    public void checkEmail(String email){
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,4})([.][a-zA-Z]{2})*$");
+        Matcher match = pattern.matcher(email);
+        if (match.matches())
+            System.out.println("Email is Valid");
+        else
+            System.out.println("Email is InValid");
     }
 }
