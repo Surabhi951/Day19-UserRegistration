47f53f66b83149c9b88ce1b93ba5871a6d5ca3c0
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-04 18:40:40 +0530

    check user last name is valid or not

--- src/com/bridgelabz/UserRegistrationRegex.java
+++ src/com/bridgelabz/UserRegistrationRegex.java
@@ -4,7 +4,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationRegex {
-
     public static void main(String[] args) {
         UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
         Scanner sc = new Scanner(System.in);
@@ -12,36 +11,29 @@ public class UserRegistrationRegex {
         System.out.println("Enter first name");
         String firstName = sc.next();
         userRegistrationRegex.checkFirstName(firstName);
-        System.out.println();
+
 
         System.out.println("Enter last name");
         String lastName = sc.next();
         userRegistrationRegex.checkLastName(lastName);
-
-     public static void main(String[] args) {
-        UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first name");
-        String firstName = sc.next();
-        userRegistrationRegex.checkFirstName(firstName);
     }
 
-    public void checkFirstName(String firstName){
-        Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
-        Matcher match = pattern.matcher(firstName);
-        if (match.matches())
-            System.out.println("First name is Valid");
-        else
-            System.out.println("First name is InValid");
-    }
+        public void checkFirstName (String firstName){
+            Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+            Matcher match = pattern.matcher(firstName);
+            if (match.matches())
+                System.out.println("First name is Valid");
+            else
+                System.out.println("First name is InValid");
+        }
 
-    public void checkLastName(String lastName){
-        Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
-        Matcher match = pattern.matcher(lastName);
-        if (match.matches())
-            System.out.println("Last name is Valid");
-        else
-            System.out.println("Last name is InValid");
-    }
-
+        public void checkLastName (String lastName){
+            Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+            Matcher match = pattern.matcher(lastName);
+            if (match.matches())
+                System.out.println("Last name is Valid");
+            else
+                System.out.println("Last name is InValid");
+        }
 }
+
