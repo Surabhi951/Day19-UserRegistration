e2571aaf3c3ac4d09506d755d7a6f76cadf2eaa5
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-04 18:26:59 +0530

    check user last name is valid or not

--- src/com/bridgelabz/UserRegistrationRegex.java
+++ src/com/bridgelabz/UserRegistrationRegex.java
@@ -1,23 +1,38 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationRegex {
-    public void checkFirstName(String name) {
-        boolean isMatched = Pattern.compile("^[A-Z][a-zA-Z]{2,}").matcher(name).matches();
-        if (isMatched) {
-            System.out.println("It is valid name");
-        } else {
-            System.out.println("It is not a valid name");
-        }
+    public static void main(String[] args) {
+        UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first name");
+        String firstName = sc.next();
+        userRegistrationRegex.checkFirstName(firstName);
+
+        System.out.println("Enter last name");
+        String lastName = sc.next();
+        userRegistrationRegex.checkLastName(lastName);
     }
 
-    public void checkLastName(String lastName) {
-        boolean isMatched = Pattern.compile("^[A-Z][a-zA-Z]{2,}").matcher(lastName).matches();
-        if (isMatched) {
-            System.out.println("It is valid name");
-        } else {
-            System.out.println("It is not a valid name");
-        }
+    public void checkFirstName(String firstName){
+        Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+        Matcher match = pattern.matcher(firstName);
+        if (match.matches())
+            System.out.println("First name is Valid");
+        else
+            System.out.println("First name is InValid");
     }
+
+    public void checkLastName(String lastName){
+        Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+        Matcher match = pattern.matcher(lastName);
+        if (match.matches())
+            System.out.println("Last name is Valid");
+        else
+            System.out.println("Last name is InValid");
+    }
+
 }
