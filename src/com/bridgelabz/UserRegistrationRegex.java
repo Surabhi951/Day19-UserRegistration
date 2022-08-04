6a6c1e5eb94321e24ef492dbde785142a843c9fa
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-04 18:08:33 +0530

    check user first name is valid or not

--- src/com/bridgelabz/UserRegistrationRegex.java
+++ src/com/bridgelabz/UserRegistrationRegex.java
@@ -1,18 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationRegex {
-    public void checkFirstName(String name)
-    {
-        boolean isMatched = Pattern.compile("^[A-Z][a-zA-Z]{2,}").matcher(name).matches();
-        if(isMatched)
-        {
-            System.out.println("It is valid name");
-        }
-        else
-        {
-            System.out.println("It is not a valid name");
-        }
+    public static void main(String[] args) {
+        UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first name");
+        String firstName = sc.next();
+        userRegistrationRegex.checkFirstName(firstName);
     }
+
+    public void checkFirstName(String firstName){
+        Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+        Matcher match = pattern.matcher(firstName);
+        if (match.matches())
+            System.out.println("First name is Valid");
+        else
+            System.out.println("First name is InValid");
+    }
+
 }
